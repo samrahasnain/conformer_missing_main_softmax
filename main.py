d208bad918f697695872f14fdc48d5a594416efa
samrahasnain <143751042+samrahasnain@users.noreply.github.com>
2023-09-29 03:24:38 +0500

    Update main.py

--- main.py
+++ main.py
@@ -150,8 +150,8 @@ if __name__ == '__main__':
     parser.add_argument('--test_folder', type=str, default='testint')  # Test results saving folder
     parser.add_argument('--sal_mode', type=str, default='RGBD135',
                         choices=['NJU2K', 'NLPR', 'STERE', 'RGBD135', 'LFSD', 'SIP', 'ReDWeb-S'])  # Test image dataset
-    #parser.add_argument('--test_root', type=str, default='../testsod/RGBD135/RGBD135')
-    #parser.add_argument('--test_list', type=str, default='../testsod/RGBD135/RGBD135/test.lst')
+    parser.add_argument('--test_root', type=str, default='../testsod/RGBD135/RGBD135')
+    parser.add_argument('--test_list', type=str, default='../testsod/RGBD135/RGBD135/test.lst')
     # Misc
     parser.add_argument('--mode', type=str, default='train', choices=['train', 'test'])
     config = parser.parse_args()
@@ -159,6 +159,6 @@ if __name__ == '__main__':
     if not os.path.exists(config.save_folder):
         os.mkdir(config.save_folder)
 
-    get_test_info(config)
+    #get_test_info(config)
 
     main(config)
